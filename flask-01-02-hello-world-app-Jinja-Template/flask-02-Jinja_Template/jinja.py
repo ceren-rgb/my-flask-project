9f880321383ed61e4d059b8ce8be99cae420f0ae
Ceren <cerencvlz.cam@gmail.com>
2023-06-25 13:22:09 +0300

    flask 1 and 2 added

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-02-Jinja_Template/jinja.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def head():
+    return render_template("index.html", number1=12565834, number2=78898096)
+
+
+@app.route("/sum")
+def number():
+    num1 = 23986
+    num2 = 57874
+    return render_template("body.html",
+                           value1=num1,
+                           value2=num2,
+                           sum=num1 + num2)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=30000)
